2ce60e68dac07dfda69aa889ee4d55fc9721c7cd
Aleksei Dubrovskii <aleksei.dubrovskii@stm-labs.ru>
2024-11-21 20:53:53 +0300

    +

--- task3.py
+++ task3.py
@@ -18,3 +18,75 @@
 
 # Метод add_device(device), который добавляет устройство в магазин.
 # Метод list_devices(), который выводит информацию обо всех устройствах.
+
+class Device:
+    def __init__(self, brand, model, price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+
+    def get_info(self):
+        return (
+            f'Производитель — {self.brand}\n'
+            f'Модель — {self.model}\n'
+            f'Цена — {self.price}'
+        )
+
+
+class Laptop(Device):
+    def __init__(self, brand, model, price, ram, storage):
+        super().__init__(brand, model, price)
+        self.ram = ram
+        self.storage = storage
+
+    def get_info(self):
+        return (
+            f'Производитель — {self.brand}\n'
+            f'Модель — {self.model}\n'
+            f'Цена — {self.price}\n'
+            f'Оперативная память — {self.ram}\n'
+            f'Объем памяти — {self.storage}'
+        )
+
+
+class Smartphone(Device):
+    def __init__(
+            self, brand, model, price,
+            camera_megapixels, battery_capacity
+            ):
+        super().__init__(brand, model, price)
+        self.camera_megapixels = camera_megapixels
+        self.battery_capacity = battery_capacity
+
+    def get_info(self):
+        return (
+            f'Производитель — {self.brand}\n'
+            f'Модель — {self.model}\n'
+            f'Цена — {self.price}\n'
+            f'Камера — {self.camera_megapixels}\n'
+            f'Емкость батареи — {self.battery_capacity}'
+        )
+
+
+class Store:
+    def __init__(self):
+        self.device = []
+
+    def add_device(self, device):
+        self.device.append(device)
+
+    def list_device(self):
+        for device in self.device:
+            print(device.get_info(), '\n')
+
+
+store = Store()
+
+laptop = Laptop("Motorolla", "Pip 1", 100, 16, 512)
+# print(laptop.get_info(), '\n')
+smartphone = Smartphone("Яблочко", "iЯблочко 11", 10000, 12, 1024)
+# print(smartphone.get_info(), '\n')
+
+store.add_device(laptop)
+store.add_device(smartphone)
+store.list_device()
